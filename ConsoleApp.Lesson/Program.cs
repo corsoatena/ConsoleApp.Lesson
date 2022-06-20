6da2287cc0aca9e95b5daf193255a7f3fabc900f
Bruno Ferreira <bruno_ferreira@hotmail.it>
2022-06-20 17:23:06 +0200

    Add project files.

---
+++ ConsoleApp.Lesson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ConsoleApp2;
+namespace ConsoleApp.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Auto auto2 = new Auto("Red");
+           // auto2.Color = "Green";
+            Console.WriteLine(auto2.Color);
+            
+        }
+        // public class Auto
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+     static void Main(string[] args)
+      {
+         Console.WriteLine("Hello world!");
+      }
+    }
+    public class Auto
+    {
+        public string Name ;
+        public string Color { get; }
+        int NRuote = 4 ;
+
+        public int NumeroRuote()
+        {
+            return NRuote;   
+        }
+
+        public Auto(string colore)
+        {
+            Color = colore;
+        }
+    }
+}   
